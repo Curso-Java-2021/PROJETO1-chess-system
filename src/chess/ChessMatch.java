bbaeec3602b8d00665cb56337b7df66136a612a3
lmarosestolato <malurosestolato.lma@gmail.com>
2021-06-03 22:53:15 -0300

    Implementacao dos movimentos possiveis da Torre

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -30,7 +30,8 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
 
-        valideSourcePosition(source);
+        validateSourcePosition(source);
+        validateTargetPosition(source, target);
 
         Piece capturedPiece = makeMove(source, target);
 
@@ -47,13 +48,21 @@ public class ChessMatch {
     }
 
     //VALIDA A POSICAO DE ORIGEM
-    private void valideSourcePosition(Position position){
+    private void validateSourcePosition(Position position){
         if(!board.thereIsAPiece(position)){
             throw new ChessException("There is no piece on source position");
         }
         if(!board.piece(position).isThereAnyPossibleMove()){
             throw new ChessException("There is no possible moves for the chosen piece");
         }
+    }
+
+    //VALIDA A POSICAO DE DESTINO
+    private void validateTargetPosition(Position source, Position target){
+        if(!board.piece(source).possibleMove(target)){
+            throw new ChessException("The chosen piece can't move to target position");
+        }
+        
     }
 
     //DEFINE A POSICAO DA NOVA PECA NO FORMATO LETRA+NUMERO
