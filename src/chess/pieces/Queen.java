f332abfc4ade83e52a99473e40eea241d2299af6
lmarosestolato <malurosestolato.lma@gmail.com>
2021-06-05 18:59:54 -0300

    Implementacao da Rainha

--- src/chess/pieces/Queen.java
+++ src/chess/pieces/Queen.java
@@ -5,9 +5,9 @@ import boardGame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-//BISPO
-public class Bishop extends ChessPiece {
-    public Bishop(Board board, Color color) {
+//RAINHA
+public class Queen extends ChessPiece {
+    public Queen(Board board, Color color) {
         super(board, color);
     }
 
@@ -16,6 +16,46 @@ public class Bishop extends ChessPiece {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 
         Position p = new Position(0, 0);
+
+        //Acima
+        p.setValues(position.getRow() - 1, position.getColumn());
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() - 1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //Esquerda
+        p.setValues(position.getRow(), position.getColumn() - 1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() - 1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //Direita
+        p.setValues(position.getRow(), position.getColumn() + 1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() + 1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //Abaixo
+        p.setValues(position.getRow() + 1, position.getColumn());
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() + 1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
 
         //Noroeste
         p.setValues(position.getRow() - 1, position.getColumn() - 1);
@@ -62,6 +102,6 @@ public class Bishop extends ChessPiece {
     
     @Override
     public String toString(){
-        return "B";
+        return "Q";
     }
 }
